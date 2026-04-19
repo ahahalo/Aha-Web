7ae3b04787667770742356b03c04d3ebfd28e5ad
Jens Kauteren <koekskeinc@gmail.com>
2026-04-19 17:09:09 +0200

    pusqj

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1776596182|517998898';
+const CACHE_VERSION = '1776611343|1531239795';
 /** @type {string} */
 const CACHE_PREFIX = 'Aha Web-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;

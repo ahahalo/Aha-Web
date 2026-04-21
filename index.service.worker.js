17f4aa777aa853dd682fb7b57b19581e0239f8b3
Jens Kauteren <koekskeinc@gmail.com>
2026-04-21 18:23:49 +0200

    fiox

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1776788571|253969764';
+const CACHE_VERSION = '1776788624|307749912';
 /** @type {string} */
 const CACHE_PREFIX = 'Aha Web-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
